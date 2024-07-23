ee736f52084c88aa53e60b3b2510f33c50578c4e
shunlog <artiombalan331@gmail.com>
2024-07-23 19:10:49 +0300

    move tests inside the module

--- main.rkt
+++ main.rkt
@@ -10,5 +10,5 @@
     (playfield-add-block (block 0 0 'L))
     (playfield-add-block* (list
                            (block 0 1 'ghost)
-                           (block 1 1 'garbage)))
+                           (block 0 1 'garbage)))
     (playfield-blocks))
--- playfield.rkt
+++ playfield.rkt
@@ -60,11 +60,31 @@
 ; Natural, Natural -> Playfield
 (define (empty-playfield w h)  (playfield w h (hash)))
 
+
+; Playfield -> List[Block]
+(define (playfield-blocks p)
+  (define hash (playfield-block-hash p))
+  (define (key-val-to-block k v)
+    (block (car k) (cadr k) v))
+  (hash-map hash key-val-to-block))
+
+
 (define (playfield-width p)
   (playfield-w p))
 
+
 (define (playfield-height p)
   (playfield-h p))
+
+
+(module+ test  
+  (test-case
+      "Create an empty Playfield"
+    (define plf0 (empty-playfield 10 20))
+    (check-equal? (playfield-blocks plf0) '())
+    (check-equal? (playfield-width plf0) 10)
+    (check-equal? (playfield-height plf0) 20))
+  )
 
 
 ; Playfield Block -> Playfield
@@ -83,17 +103,60 @@
      (struct-copy playfield p
                   [block-hash new-hash])]))
 
+
+(module+ test
+  (test-case
+      "Add a block to the Playfield"
+    (define pl0 (empty-playfield 10 20))
+
+    ; Add a single block
+    (define pl1 (playfield-add-block pl0 (block 1 1 'L)))
+    (check-equal?
+     (playfield-blocks pl1)
+     (list (block 1 1 'L)))
+
+    ; Add two blocks
+    (define pl2 (playfield-add-block
+                 (playfield-add-block pl0 (block 1 1 'L))
+                 (block 1 2 'J)))
+    (check-true
+     (set=?  ;; we don't care about the order, so we create sets instead of lists
+      (apply set (playfield-blocks pl2))
+      (set (block 1 1 'L) (block 1 2 'J))))
+    
+    ;; Expect error when thereis a blocks at given position already
+    (define pl3 (playfield-add-block pl0 (block 1 1 'L)))
+    (check-exn
+     #rx"position.*1.*1"
+     (λ () (playfield-add-block pl3 (block 1 1 'J))))
+    ))
+
+
 ; Playifeld List[Block] -> Playfield
 (define (playfield-add-block* p bl)
   (foldl (λ (b p) (playfield-add-block p b)) p bl))
 
-; Playfield -> List[Block]
-(define (playfield-blocks p)
-  (define hash (playfield-block-hash p))
-  (define (key-val-to-block k v)
-    (block (car k) (cadr k) v))
-  (hash-map hash key-val-to-block))
 
+(module+ test
+  (test-case
+      "Add a list of blocks"
+    (define pl0 (empty-playfield 10 20))
+    
+    ; Add list of blocks
+    (define bl1 (list (block 0 1 'J)
+                      (block 1 2 'S)))
+    (define pl2 (playfield-add-block* pl0 bl1))
+    (check-not-false
+     (set=?
+      (apply set (playfield-blocks pl2))
+      (apply set bl1)))
+    
+    ;; Error if any block fails to be added (so none will be added)
+    (check-exn
+     #rx"position.*1.*2"
+     (λ () (playfield-add-block* pl0 (list (block 1 2 'L)
+                                           (block 1 2 'J)))))
+    ))
 
 
 ; Playfield -> Playfield
