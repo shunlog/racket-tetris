d9d96ad5b7b43471eb0066d6893a0bfdde8f1741
arbalan33 <arbalan@griddynamics.com>
2025-06-03 14:49:48 +0300

    add dependencies

--- info.rkt
+++ info.rkt
@@ -1,9 +1,9 @@
 #lang info
 (define collection "racket-tetris")
-(define deps '("base"))
+(define deps '("base" "threading" "memo"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/racket-tetris.scrbl" ())))
-(define pkg-desc "Description Here")
+(define pkg-desc "Full Tetris implementation")
 (define version "0.0")
 (define pkg-authors '(awh))
 (define license '(Apache-2.0 OR MIT))
